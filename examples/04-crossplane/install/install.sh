ab6e829d5a2e483bbf6af13567d3aa5ca3cfb8f3
Frederique <fretsema@fretsema.nl>
2026-04-22 14:48:38 +0200

    04-crossplane

--- examples/04-crossplane/install/install.sh
+++ examples/04-crossplane/install/install.sh
@@ -37,10 +37,131 @@ function install_aws_secrets() {
   rm /tmp/$$
 }
 
-function load_scripts() {
+function wait_for_full_line() {
+    SCRIPT=$1
+    EXPECTED_WORDCOUNT=$2
+
+    OUTPUT=$(kubectl get -f "$SCRIPT" | tail -n 1)
+    REAL_WORDCOUNT=$(echo "$OUTPUT" | wc -w)
+    while [[ "$EXPECTED_WORDCOUNT" != "$REAL_WORDCOUNT" ]]
+    do
+        echo "Output: $OUTPUT"
+        echo "Expected wordcount $EXPECTED_WORDCOUNT != Real wordcount $REAL_WORDCOUNT"
+        echo "Wait 2 seconds..."
+        sleep 2
+        OUTPUT=$(kubectl get -f "$SCRIPT" | tail -n 1)
+        REAL_WORDCOUNT=$(echo "$OUTPUT" | wc -w)
+    done
+}
+
+function wait_for_healthy() {
+    SCRIPTNAME=$1
+
+    OUTPUT=$(kubectl get -f "$SCRIPTNAME" | tail -n 1)
+    HEALTHY=$(echo "$OUTPUT" | awk '{print $3}')
+    while [[ "$HEALTHY" != "True" ]]
+    do
+        echo "Output: $OUTPUT"
+        echo "Expected health True != Real health $HEALTHY"
+        echo "Wait 2 seconds..."
+        sleep 2
+        OUTPUT=$(kubectl get -f "$SCRIPTNAME" | tail -n 1)
+        HEALTHY=$(echo "$OUTPUT" | awk '{print $3}')
+    done
+    echo "$SCRIPTNAME = Healthy"
+}
+
+function wait_for_established() {
+    SCRIPTNAME=$1
+
+    OUTPUT=$(kubectl get -f "$SCRIPTNAME" | tail -n 1)
+    ESTABLISHED=$(echo "$OUTPUT" | awk '{print $2}')
+    while [[ "$ESTABLISHED" != "True" ]]
+    do
+        echo "Output: $OUTPUT"
+        echo "Expected established True != Real established $ESTABLISHED"
+        echo "Wait 2 seconds..."
+        sleep 2
+        OUTPUT=$(kubectl get -f "$SCRIPTNAME" | tail -n 1)
+        ESTABLISHED=$(echo "$OUTPUT" | awk '{print $2}')
+    done
+    echo "$SCRIPTNAME = Established"
+}
+
+function wait_for_composition() {
+    SCRIPTNAME=$1
+
+    OUTPUT=$(kubectl get -f "$SCRIPTNAME" | tail -n 1)
+    DEPLOYED=$(echo "$OUTPUT" | grep "not found")
+    while [[ "$DEPLOYED" != "" ]]
+    do
+        echo "Output: $OUTPUT"
+        echo "Expected output (no message 'not found') != Real health $OUTPUT"
+        echo "Wait 2 seconds..."
+        kubectl apply -f "$SCRIPTNAME"
+        sleep 2
+        OUTPUT=$(kubectl get -f "$SCRIPTNAME" | tail -n 1)
+        DEPLOYED=$(echo "$OUTPUT" | grep "not found")
+    done
+    echo "$SCRIPTNAME = Deployed"
+}
+
+function wait_for_ready() {
+    SCRIPTNAME=$1
+
+    OUTPUT=$(kubectl get -f "$SCRIPTNAME" | tail -n 1)
+    READY=$(echo "$OUTPUT" | awk '{print $3}')
+    while [[ "$READY" != "True" ]]
+    do
+        echo "Output: $OUTPUT"
+        echo "Expected health True != Real health $READY"
+        echo "Wait 2 seconds..."
+        sleep 2
+        OUTPUT=$(kubectl get -f "$SCRIPTNAME" | tail -n 1)
+        READY=$(echo "$OUTPUT" | awk '{print $3}')
+    done
+    echo "$SCRIPTNAME = Ready"
+}
+
+deploy_function() {
+    SCRIPTNAME="01-fn.yml"
+
+    kubectl apply -f "$SCRIPTNAME"
+    wait_for_full_line "$SCRIPTNAME" 5
+    wait_for_healthy "$SCRIPTNAME"
+}
+
+function deploy_xrd() {
+    SCRIPTNAME="02-xrd.yml"
+
+    kubectl apply -f "$SCRIPTNAME"
+    wait_for_full_line "$SCRIPTNAME" 3
+    wait_for_established "$SCRIPTNAME"
+}
+
+function deploy_composition() {
+    SCRIPTNAME="03-composition.yml"
+
+    kubectl apply -f "$SCRIPTNAME"
+    wait_for_composition "$SCRIPTNAME"
+}
+
+function deploy_app() {
+    SCRIPTNAME="04-app.yml"
+
+    kubectl apply -f "$SCRIPTNAME"
+    wait_for_full_line "$SCRIPTNAME" 5
+    wait_for_ready "$SCRIPTNAME"
+}
+
+function load_scripts_composition() {
     DIR=$1
     cd $DIR
-    ls -1 | sort | awk '{print "kubectl create -n 04-crossplane -f "$1}' | bash
+
+    deploy_function
+    deploy_xrd
+    deploy_composition
+    deploy_app
     cd -
 }
 
@@ -49,4 +170,4 @@ install_aws_secrets
 
 # Only load scripts that don't create AWS resources (they will stay in AWS when
 # you delete the cluster without deleting the resources within Kubernetes first)
-load_scripts /clone/Kubecon26-example-repo/examples/04-crossplane/composition
+load_scripts_composition
